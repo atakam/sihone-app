d97e14f3fb27f1bd8358f71dae90cdd644035fa9
Austin Takam <austin.takam@ibm.com>
2019-02-06 11:29:42 -0500

    relocate static reference

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const app = require('./app');
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`listening on port ${port}`));
 
