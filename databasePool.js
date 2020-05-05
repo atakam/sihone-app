35c431086c7a5eafc3ea28e4b412de7461150cdb
Austin Takam <austin.takam@sihone.com>
2020-05-05 14:20:20 -0400

    Update databasePool.js

--- databasePool.js
+++ databasePool.js
@@ -1,9 +1,9 @@
 const { Pool } = require('pg');
 const databaseConfiguration = {
-    user: 'wgyvwcfk',
-    host: 'elmer.db.elephantsql.com', //'churchbraindb.c6nccd29fuwa.us-east-2.rds.amazonaws.com',
-    database: 'wgyvwcfk',
-    password: 'Ecz0voOUXOqgFG8gd8oQuaArpGiMqoSX',
+    user: 'knlnsgle',
+    host: 'baasu.db.elephantsql.com', //'churchbraindb.c6nccd29fuwa.us-east-2.rds.amazonaws.com',
+    database: 'knlnsgle',
+    password: '6Sh52hcm3kCmfGVoR1x79T0GH1plFZ7c',
     port: 5432
 };
 
