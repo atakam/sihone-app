a8cc31d5e1b2d6e4e6b19c025f67ac949e884c29
Austin Takam <austin.takam@ibm.com>
2020-05-06 15:15:21 -0400

    Adding AppBar

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -3,15 +3,8 @@ import classNames from "classnames";
 import PropTypes from "prop-types";
 // @material-ui/core components
 import withStyles from "@material-ui/core/styles/withStyles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import IconButton from "@material-ui/core/IconButton";
-import Hidden from "@material-ui/core/Hidden";
-// @material-ui/icons
-import Menu from "@material-ui/icons/Menu";
 // core components
-import HeaderLinks from "./HeaderLinks";
-import Button from "components/CustomButtons/Button";
+import CustomAppBar from "./CustomAppBar";
 
 import headerStyle from "assets/jss/material-dashboard-react/components/headerStyle.jsx";
 
@@ -31,32 +24,39 @@ function Header({ ...props }) {
     [" " + classes[color]]: color
   });
   return (
-    <AppBar className={classes.appBar + appBarClasses}>
-      <Toolbar className={classes.container}>
-        <div className={classes.flex}>
-          {/* Here we create navbar brand, based on route name */}
-          <Button color="transparent" href="#" className={classes.title}>
-            {makeBrand()}
-          </Button>
-        </div>
-        <Hidden smDown implementation="css">
-          <HeaderLinks
-            name={props.name}
-            openMemberDialog={props.openMemberDialog}
-            openNewMemberDialog={props.openNewMemberDialog}
-          />
-        </Hidden>
-        <Hidden mdUp implementation="css">
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={props.handleDrawerToggle}
-          >
-            <Menu />
-          </IconButton>
-        </Hidden>
-      </Toolbar>
-    </AppBar>
+    <CustomAppBar
+      title={makeBrand()}
+      name={props.name}
+      openMemberDialog={props.openMemberDialog}
+      openNewMemberDialog={props.openNewMemberDialog}
+    />
+    // <AppBar className={classes.appBar + appBarClasses}>
+    //   <Toolbar className={classes.container}>
+    //     <div className={classes.flex}>
+    //       {/* Here we create navbar brand, based on route name */}
+    //       <Button color="transparent" href="#" className={classes.title}>
+    //         {'JadeSoft > '}
+    //         <strong>&nbsp;{makeBrand()}</strong>
+    //       </Button>
+    //     </div>
+    //     <Hidden smDown implementation="css">
+    //       <CustomAppBar
+    //         name={props.name}
+    //         openMemberDialog={props.openMemberDialog}
+    //         openNewMemberDialog={props.openNewMemberDialog}
+    //       />
+    //     </Hidden>
+    //     <Hidden mdUp implementation="css">
+    //       <IconButton
+    //         color="inherit"
+    //         aria-label="open drawer"
+    //         onClick={props.handleDrawerToggle}
+    //       >
+    //         <Menu />
+    //       </IconButton>
+    //     </Hidden>
+    //   </Toolbar>
+    // </AppBar>
   );
 }
 
