b60274720950f85fb2fc834a41b9613eab302c83
Austin Takam <austin.takam@sihone.com>
2020-05-08 10:40:20 -0400

    Update databasePool.js

--- databasePool.js
+++ databasePool.js
@@ -1,25 +1,12 @@
 const { Pool } = require('pg');
 const databaseConfiguration = {
-    user: 'sctbckje',
+    user: 'knlnsgle',
     host: 'baasu.db.elephantsql.com', //'churchbraindb.c6nccd29fuwa.us-east-2.rds.amazonaws.com',
-    database: 'sctbckje',
-    password: 'N6gW0bjDrYfn-i_Un1w6HY8mhvv_u84j',
+    database: 'knlnsgle',
+    password: '6Sh52hcm3kCmfGVoR1x79T0GH1plFZ7c',
     port: 5432
 };
 
 const pool = new Pool(databaseConfiguration);
 
 module.exports = pool;
-
-// const { Pool } = require('pg');
-// const databaseConfiguration = {
-//     user: 'wgyvwcfk',
-//     host: 'elmer.db.elephantsql.com', //'churchbraindb.c6nccd29fuwa.us-east-2.rds.amazonaws.com',
-//     database: 'wgyvwcfk',
-//     password: 'Ecz0voOUXOqgFG8gd8oQuaArpGiMqoSX',
-//     port: 5432
-// };
-
-// const pool = new Pool(databaseConfiguration);
-
-// module.exports = pool;
